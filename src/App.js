6053c5517a568952166fb04192c7592284db44eb
rev bar <revital.jer@gmail.com>
2021-01-05 21:27:55 +0200

    bootstrap install+adding input

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Card, Container, Form, FormControl } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+    <Container>
+      <Form.Group controlId="toDo">
+          <Form.Label>toDos</Form.Label>
+          <Form.Control type="text" placeholder="What's next?" />
+      </Form.Group>
+    </Container>
+
     </div>
   );
 }
